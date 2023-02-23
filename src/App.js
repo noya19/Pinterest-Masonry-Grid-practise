d41517b01776bf552cbb97227aebb03cf0f588b5
Ayon <ayonsharma1999@gmail.com>
2023-02-23 16:50:00 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+
+const imgArr = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16, 17, 18, 1, 20,
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="pinterest_container">
+      <div class="columns-1 md:columns-2 lg:columns-3 ">
+        {imgArr.map((cur) => {
+          return (
+            <img
+              class="image_element"
+              key={cur}
+              alt={cur}
+              src={`https://source.unsplash.com/random/architecture-interior&${
+                Math.random() * 1000
+              }`}
+            ></img>
+          );
+        })}
+      </div>
     </div>
   );
 }
